a24ba5b2881df0df7e60e29209327f122d9a750b
Stephen Justin <stevejustin1963@gmail.com>
2022-05-03 23:03:46 +1000

    Update distance.c

--- distance.c
+++ distance.c
@@ -29,26 +29,3 @@ int main()
   return 0;
 
 }
-
-
-// or another prog
-
-#include <stdio.h> 
-#include <math.h>
-
-int main() 
-{ 
-
-float dist; 
-float time; 
-
-printf("Enter the time taken for the sound waves to bounce back: "); 
-scanf("%f", &time); 
-
-dist = (time * 340) / 2; 
-
-printf("The distance to the object is %f m.", dist); 
-
-return 0; 
-}
-
